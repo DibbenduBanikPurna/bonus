8e86ec46a8ec1eb0c792888a2087ede4452e27da
DibbenduBanikPurna <77313185+DibbenduBanikPurna@users.noreply.github.com>
2021-02-25 22:05:29 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import Button from '@material-ui/core/Button';
 function App() {
+  const [like,setLike]=useState('')
+  const [user,setUser]=useState([])
+  const [person,setPerson]=useState({})
+  const [random,setRandom]=useState({})
+ const handleLike=()=>{
+   setLike(like ? '': 'primary' )
+ }
+ useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res=>res.json())
+  .then(data=>{
+    setUser(data)
+  })
+
+
+  fetch('https://jsonplaceholder.typicode.com/users/1')
+  .then(res=>res.json())
+  .then(data=>setPerson(data))
+
+
+  fetch('https://randomuser.me/api/')
+  .then(res=>res.json())
+  .then(data=>{
+    setRandom(data.results[0])
+  })
+ },[])
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Hi baby fuck</h2>
+     
+     <Button onClick={()=>setLike(handleLike)} color={like}><ThumbUpAltIcon/></Button>
+     <h2>{person.name}</h2>
+     <h5>{random.gender}</h5>
+     <p>{random.name && random.name.title} </p>
+     {
+       user.map((user)=>{
+         return <li key={user.id}>{user.name}</li>
+       })
+     }
     </div>
   );
 }
